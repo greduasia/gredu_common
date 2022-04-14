e48727a1b87b3a7225ac8e842c92d7974d0be815
Wahyu Adhe Triwibowo <wahyu.adhe@gredu.asia>
2022-04-14 16:30:20 +0700

    feat(Unit Test/Logging): [GK12-2016] init

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,12 +5,25 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
       title: 'Application',
       initialRoute: AppPages.INITIAL,
       debugShowCheckedModeBanner: false,
       getPages: AppPages.routes,
-    ),
-  );
+    );
+  }
 }
--- test/features/ex_logger_test.dart
+++ test/features/ex_logger_test.dart
@@ -1,1 +1,92 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gredu_common/gredu_common.dart';
 
+Future<void> main() async {
+
+  await ExLog.init();
+
+  /// Matcher Type:
+  /// isEmpty
+  /// isNotEmpty
+  /// isNull
+  /// isNotNull
+  /// isTrue
+  /// isFalse
+  /// isNaN
+  /// isNotNaN
+  /// anything
+  /// returnsNormally
+
+  group('Check log(logDebug)', () {
+
+    setUp(() async {
+      ExLog.init();
+    });
+
+    test('check if the `log` is working normally', () {
+      expect(() => log('this is a message'), returnsNormally);
+    });
+
+  });
+
+  group('Check print(logDebug)', () {
+
+    setUp(() async {
+      ExLog.init();
+    });
+
+    test('check if the `print` is working normally', () {
+      expect(() => log('this is a message'), returnsNormally);
+    });
+
+  });
+
+  group('Check logD(logDebug)', () {
+
+    setUp(() async {
+      ExLog.init();
+    });
+
+    test('check if the `logD` is working normally', () {
+      expect(() => logD('this is a message'), returnsNormally);
+    });
+
+  });
+
+  group('Check logE(logError)', () {
+
+    setUp(() async {
+      ExLog.init();
+    });
+
+    test('check if the `logE` is working normally', () {
+      expect(() => logE('this is a message'), returnsNormally);
+    });
+
+  });
+  
+  group('Check logI(logInfo)', () {
+
+    setUp(() async {
+      ExLog.init();
+    });
+
+    test('check if the `logI` is working normally', () {
+      expect(() => logI('this is a message'), returnsNormally);
+    });
+
+  });
+    
+  group('Check logW(logWarning)', () {
+
+    setUp(() async {
+      ExLog.init();
+    });
+
+    test('check if the `logW` is working normally', () {
+      expect(() => logW('this is a message'), returnsNormally);
+    });
+
+  });
+  
+}
